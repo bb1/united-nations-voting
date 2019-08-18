5208fce40dfd6078687bffa5574c76ef3af4c507
bb1 <3842623+bb1@users.noreply.github.com>
2019-08-18 18:54:56 +0200

    query optimized. takes 11s (or even less then 1s) instead of 20m

--- vote_parser/db/matrix_query_optimized.sql
+++ vote_parser/db/matrix_query_optimized.sql
@@ -1,11 +1,3 @@
-WITH votes AS (
-	SELECT 
-		resolution_id,
-		country,
-		yes
-	FROM un.vote
-	WHERE (yes or no)
-)
 SELECT 
 	c1 as country1,
 	c2 as country2,
@@ -15,8 +7,22 @@ FROM
         SELECT
             v1.country as c1,
             v2.country as c2
-        FROM votes v1
-        JOIN votes v2 ON
+        FROM (
+	SELECT 
+		resolution_id,
+		country,
+		yes
+	FROM un.vote
+	WHERE (yes or no)
+) v1
+        JOIN (
+	SELECT 
+		resolution_id,
+		country,
+		yes
+	FROM un.vote
+	WHERE (yes or no)
+) v2 ON
             v1.resolution_id = v2.resolution_id AND v1.yes = v2.yes
 	        AND v1. country <> v2. country
 ) a
